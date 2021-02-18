91a83b8dc0f71a9d36d93ee6adaa66dcf48aa6e6
Abhi <abhishekyerram02@gmail.com>
2021-02-18 14:29:14 +0530

    Committing Debug Code v1

---
+++ src/Counter.java
@@ -0,0 +1,16 @@
+
+public class Counter {
+	int result=0;
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		System.out.println("Set");
+		this.result = result;
+	}
+	
+	
+
+}
